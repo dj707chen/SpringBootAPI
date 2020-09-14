f708319f4111e55f321bfacf875aa0f58f09cae1
weipingc <weiping.chen@gmail.com>
2020-09-13 21:41:12 -0500

    try docker-compose, but still does not work

---
+++ docker/docdn.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Run docker stack deploy -c stack.yml postgres (or docker-compose -f stack.yml up), 
+# wait for it to initialize completely, and visit 
+# http://swarm-ip:7000, http://localhost:7000, or http://host-ip:7000 (as appropriate).
+
+# docker stack deploy -c stack.yml postgres
+# docker-compose -f stack.yml down
+docker-compose down
+
